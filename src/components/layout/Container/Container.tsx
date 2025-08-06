5241cfd4927b2f89fa7dc5ae5c86e3508b8a070b
selosele <osh3384@naver.com>
2025-08-06 11:49:36 +0900

    wrapper 클래스 설정

--- src/components/layout/Container/Container.tsx
+++ src/components/layout/Container/Container.tsx
@@ -2,5 +2,7 @@ import type { ContainerProps } from '@/types/container'
 import './Container.css'
 
 export default function Container(props: ContainerProps) {
-    return <div className='container'>{props.children}</div>
+    return (
+        <div className={`container ${props.className}`}>{props.children}</div>
+    )
 }
--- src/components/layout/Wrapper/Wrapper.tsx
+++ src/components/layout/Wrapper/Wrapper.tsx
@@ -2,12 +2,13 @@ import { Outlet } from 'react-router-dom'
 import Header from '../Header/Header'
 import Container from '../Container/Container'
 import Footer from '../Footer/Footer'
+import './Wrapper.css'
 
 export default function Wrapper() {
     return (
         <>
             <Header />
-            <Container>
+            <Container className='wrapper'>
                 <Outlet />
             </Container>
             <Footer />
