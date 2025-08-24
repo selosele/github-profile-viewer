9d2096ed35c067b704c2d9ee6f9a71e29d2b9382
selosele <osh3384@naver.com>
2025-08-24 13:38:45 +0900

    코드 정리

--- src/pages/Search/SearchUser.tsx
+++ src/pages/Search/SearchUser.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { useUserStore } from '@/stores/userStore'
 import useFetchUser from '@/hooks/useFetchUser'
 
-export default function SearchUser({ userName }) {
+export default function SearchUser({ userName }: { userName: string }) {
     const { data, isLoading, isError } = useFetchUser(userName)
     const { setUser } = useUserStore()
 
@@ -16,70 +16,67 @@ export default function SearchUser({ userName }) {
             {isError && <p>Error fetching user. Check the username.</p>}
 
             {!isLoading && !isError && (
-                <>
-                    <div className='profile'>
-                        <div className='avatar'>
-                            <a href={data.avatar_url} target='_blank'>
-                                <img
-                                    src={data.avatar_url}
-                                    alt={data.login}
-                                    className='img'
-                                />
+                <div className='profile'>
+                    <div className='avatar'>
+                        <a href={data.avatar_url} target='_blank'>
+                            <img
+                                src={data.avatar_url}
+                                alt={data.login}
+                                className='img'
+                            />
+                        </a>
+                    </div>
+                    <div className='info'>
+                        <h2 className='title'>{data.name || data.login}</h2>
+                        <div className='about'>
+                            {data.location && (
+                                <p className='text'>
+                                    <i className='xi-maker'></i> {data.location}
+                                </p>
+                            )}
+                            <a
+                                href={data.html_url}
+                                target='_blank'
+                                className='text'
+                            >
+                                <i className='xi-github'></i>
+                                <span>{data.login}</span>
                             </a>
-                        </div>
-                        <div className='info'>
-                            <h2 className='title'>{data.name || data.login}</h2>
-                            <div className='about'>
-                                {data.location && (
-                                    <p className='text'>
-                                        <i className='xi-maker'></i>{' '}
-                                        {data.location}
+                            <div className='text-box'>
+                                <p>{data.bio || 'No bio available'}</p>
+                                {data.blog && (
+                                    <p>
+                                        Website:{' '}
+                                        <a
+                                            href={`https://${data.blog}`}
+                                            target='_blank'
+                                        >
+                                            {data.blog}
+                                        </a>
                                     </p>
                                 )}
-                                <a
-                                    href={data.html_url}
-                                    target='_blank'
-                                    className='text'
-                                >
-                                    <i className='xi-github'></i>
-                                    <span>{data.login}</span>
-                                </a>
-                                <div className='text-box'>
-                                    <p>{data.bio || 'No bio available'}</p>
-                                    {data.blog && (
-                                        <p>
-                                            Website:{' '}
-                                            <a
-                                                href={`https://${data.blog}`}
-                                                target='_blank'
-                                            >
-                                                {data.blog}
-                                            </a>
-                                        </p>
-                                    )}
-                                    <div className='num-box'>
-                                        <p>
-                                            Following:{' '}
-                                            <strong>{data.following}</strong>
-                                        </p>
-                                        <p>
-                                            Followers:{' '}
-                                            <strong>{data.followers}</strong>
-                                        </p>
-                                        <p>
-                                            Repos:{' '}
-                                            <strong>{data.public_repos}</strong>
-                                        </p>
-                                        <p>
-                                            Gists:{' '}
-                                            <strong>{data.public_gists}</strong>
-                                        </p>
-                                    </div>
+                                <div className='num-box'>
+                                    <p>
+                                        Following:{' '}
+                                        <strong>{data.following}</strong>
+                                    </p>
+                                    <p>
+                                        Followers:{' '}
+                                        <strong>{data.followers}</strong>
+                                    </p>
+                                    <p>
+                                        Repos:{' '}
+                                        <strong>{data.public_repos}</strong>
+                                    </p>
+                                    <p>
+                                        Gists:{' '}
+                                        <strong>{data.public_gists}</strong>
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </>
+                </div>
             )}
         </>
     )
