c98fcc978cb035bcef7b402337da841abcdcceef
selosele <osh3384@naver.com>
2025-08-30 15:06:16 +0900

    vercel 빌드 오류 수정

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,7 @@
-import Wrapper from './Wrapper/Wrapper'
-import Container from './Container/Container'
-import Box from './Box/Box'
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
+import Wrapper from './Wrapper/Wrapper.tsx'
+import Container from './Container/Container.tsx'
+import Box from './Box/Box.tsx'
+import Header from './Header/Header.tsx'
+import Footer from './Footer/Footer.tsx'
 
 export const Layout = { Wrapper, Container, Box, Header, Footer }
