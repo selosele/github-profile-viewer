6c09984bc5226f0d7a137dd17b428a8ac4b2f44c
selosele <osh3384@naver.com>
2025-08-30 15:46:38 +0900

    파라미터 누락 수정

--- api/github-user-repository.ts
+++ api/github-user-repository.ts
@@ -11,9 +11,12 @@ export default async function handler(req: Request) {
         })
     }
 
+    const sort = url.searchParams.get('sort')
+    const per_page = url.searchParams.get('per_page')
+
     const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN!
     const response = await fetch(
-        `https://api.github.com/users/${userName}/repos`,
+        `https://api.github.com/users/${userName}/repos?sort=${sort}&per_page=${per_page}`,
         {
             headers: {
                 Authorization: `token ${GITHUB_ACCESS_TOKEN}`,
