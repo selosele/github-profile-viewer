39f8f9ac2339ed6fa049fe944836641604ea258d
selosele <osh3384@naver.com>
2025-08-30 15:14:21 +0900

    vercel 빌드 오류 수정

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,6 +1,6 @@
-import Input from './Input/Input'
-import Select from './Select/Select'
-import Checkbox from './Checkbox/Checkbox'
-import CheckboxGroup from './Checkbox/CheckboxGroup'
+import Input from '@/components/ui/Input/Input'
+import Select from '@/components/ui/Select/Select'
+import Checkbox from '@/components/ui/Checkbox/Checkbox'
+import CheckboxGroup from '@/components/ui/Checkbox/CheckboxGroup'
 
 export const UI = { Input, Select, Checkbox, CheckboxGroup }
